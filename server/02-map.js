9caf7a3ab81960d66f8b04cec0bd1f4251a7e02a
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-05-31 10:32:04 -0500

    Explanation Map

--- server/02-map.js
+++ server/02-map.js
@@ -1,5 +1,7 @@
 const letters = ['a','b','c'];
 
+
+// El método de map transforma el array original y crea uno nuevo con las modificaciones de cada elementp
 const newArray = letters.map(item => item + '++')
 // const newArray = [];
 // for (let index = 0; index < letters.length; index++) {
